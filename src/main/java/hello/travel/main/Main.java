df8930f286bb6a30e2bedaef7e50996b5d365b0d
sushilpokharel <sushilpokharel@192.168.1.4>
2018-09-25 10:17:51 -0500

    second commit

--- src/main/java/hello/travel/main/Main.java
+++ src/main/java/hello/travel/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("hello there, is there any problem?");
+		System.out.println("hello ......................?");
 
 
 	}
