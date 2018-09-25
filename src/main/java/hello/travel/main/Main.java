b2c8c57943394d38bfabcd5228db8a853d30ea98
sushilpokharel <sushilpokharel@192.168.1.4>
2018-09-25 08:54:32 -0500

    hello

---
+++ src/main/java/hello/travel/main/Main.java
@@ -0,0 +1,13 @@
+package hello.travel.main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello there, is there any problem?");
+
+
+	}
+
+}
